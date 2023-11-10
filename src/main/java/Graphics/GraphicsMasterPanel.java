000749af1177893956f051a1ed50f86394b492a6
Gabriel Santos <k1913184@students.katyisd.org>
2023-11-09 21:09:05 -0600

    PDF Download works

--- src/main/java/Graphics/GraphicsMasterPanel.java
+++ src/main/java/Graphics/GraphicsMasterPanel.java
@@ -6,6 +6,10 @@ import java.awt.event.MouseListener;
 import java.awt.image.*;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 import java.util.Objects;
 
 public class GraphicsMasterPanel extends JPanel implements MouseListener {
@@ -24,18 +28,20 @@ public class GraphicsMasterPanel extends JPanel implements MouseListener {
     public void paint(Graphics g){
         super.paint(g);;
         //switch(choice){
-            /*case 1: */g.drawImage(start, 0, 0, 1920, 1080, null);
-       // }
+        /*case 1: */g.drawImage(start, 0, 0, 1366, 768, null);
+        // }
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        System.out.println("Clicked");
-        System.out.println(e.getX() + " " + e.getY());
         int x = e.getX();
         int y = e.getY();
-        if (x >= 0 && x <= 409 && y >= 0 && y <= 171){
+        if (x >= 0 && x <= 295 && y >= 0 && y <= 124){
             System.out.println("Downloaded Rules");
+            downloadRules();
+        }
+        if(x>=443 && x<=924 && y>=562 && y<=706){
+            System.out.println("Game Started!");
         }
     }
 
@@ -58,5 +64,39 @@ public class GraphicsMasterPanel extends JPanel implements MouseListener {
     public void mouseExited(MouseEvent e) {
 
     }
+    private static void downloadUsingStream(String urlStr, String file) throws IOException{
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+    }
 
+    private static void downloadUsingNIO(String urlStr, String file) throws IOException {
+        URL url = new URL(urlStr);
+        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+        fos.close();
+        rbc.close();
+    }
+    private void downloadRules(){
+        String url = "https://cmon-files.s3.amazonaws.com/pdf/assets_item/resource/126/Rulebook_Gizmos.pdf";
+        String home = System.getProperty("user.home");
+
+        try {
+            downloadUsingNIO(url, home+"/Downloads/GizmosRules.pdf");
+
+            downloadUsingStream(url, home+"/Downloads/GizmosRules.pdf");
+            Desktop.getDesktop().open(new File(home+"/Downloads/GizmosRules.pdf"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
